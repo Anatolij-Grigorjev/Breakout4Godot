63a8571a7104437f09cfe7f7dc1ccff87300dcd5
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-04-17 10:46:36 +0300

    add tile gone on hit test

--- game/ball/Ball.gd
+++ game/ball/Ball.gd
@@ -8,7 +8,8 @@ export(Vector2) var direction := Vector2.LEFT;
 func _process(delta: float):
 	var collision: KinematicCollision2D = move_and_collide(direction * speed * delta)
 	if (collision):
+		print(collision)
 		var tilemap: TileMap = collision.collider as TileMap
-		var tileMapIdx = tilemap.world_to_map(collision.position)
+		var tileMapIdx = tilemap.world_to_map(tilemap.to_local(collision.position))
 		tilemap.set_cellv(tileMapIdx, TileMap.INVALID_CELL)
 		direction = direction.bounce(collision.normal)
--- game/tests/brick_collide_tests.gd
+++ game/tests/brick_collide_tests.gd
@@ -1,23 +1,59 @@
 extends "res://addons/gut/test.gd"
 
-var GameScn = load("res://BreakoutGame.tscn")
 var BallScn = load("res://ball/Ball.tscn")
 var BricksScn = load("res://bricks/BricksMap.tscn")
 
-func test_ball_collide_bricks():
+var parent: Node2D
+var bricks
+var ball
 
-    var parent = Node2D.new()
-    var bricks = BricksScn.instance();
-    var ball = BallScn.instance()
+func before_each():
+    parent = Node2D.new()
+    bricks = BricksScn.instance();
+    ball = BallScn.instance()
+
+
+func test_ball_collide_bricks():
+    
     # bricks and ball dont touch
-    parent.add_child(bricks);
-    bricks.position = Vector2(100, 0)
-    parent.add_child(ball);
-    ball.position = Vector2(10, 0)
-    add_child_autofree(parent)
-    yield(yield_for(0.1, "waiting for parent to settle in..."), YIELD)
+    _setup_tree_bricks_pos_ball_pos(
+        Vector2(100, 0), 
+        Vector2(10, 0)
+    )
+
     #move ball into bricks
     var collision = ball.move_and_collide(Vector2(100, 0))
     assert_not_null(collision, "ball and bricks should collide!")
-    assert_eq(collision.collider, bricks, "ball should collide with bricks!")
+    if (collision):
+        assert_eq(collision.collider, bricks, "ball should collide with bricks!")
     
+
+func test_ball_collide_brick_lost():
+    # bricks and ball dont touch
+    _setup_tree_bricks_pos_ball_pos(
+        Vector2(100, 0), 
+        Vector2(10, 0)
+    )
+
+    ball.speed = 100.0
+    ball.direction = Vector2.RIGHT
+    gut.simulate(ball, 100, 0.1)
+    #top left tile
+    var tileCoord = Vector2.ZERO
+    assert_eq(bricks.get_cellv(tileCoord), TileMap.INVALID_CELL)
+
+
+func _setup_tree_bricks_pos_ball_pos(bricksPos: Vector2, ballPos: Vector2):
+    parent.add_child(bricks);
+    bricks.position = bricksPos
+    parent.add_child(ball);
+    ball.position = ballPos
+    add_child_autofree(parent)
+    _wait_next_frame()
+
+
+func _wait_seconds(secs: float, message: String = "waiting..."):
+    yield(yield_for(secs, message), YIELD)
+
+func _wait_next_frame():
+    _wait_seconds(0.16, "waiting for next frame...")
