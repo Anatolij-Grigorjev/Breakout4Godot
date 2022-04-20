bc144cb542c636c23f296d72e218bd67951018b0
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-04-20 18:41:07 +0300

    adjusted brick explode animations and particles

---
+++ game/BreakoutGame.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+onready var ball = $Ball
+onready var ballSmoke = $CollideSmoke
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ball.connect("ball_collided", self, "_on_ball_collided")
+
+
+func _on_ball_collided(collision: KinematicCollision2D):
+	
+	ballSmoke.global_position = collision.position - (collision.normal * 10)
+	ballSmoke.rotation = collision.normal.angle()
+	ballSmoke.emitting = true
