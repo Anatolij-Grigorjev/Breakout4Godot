df9aae427f6c445120ee583e9b9d6f3673a5d15e
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-04-28 18:32:25 +0300

    add points counter with numbers tween

--- game/BreakoutGame.gd
+++ game/BreakoutGame.gd
@@ -2,15 +2,21 @@ extends Node2D
 
 export(Vector2) var initialBallPos = Vector2(190, 200)
 
+onready var bricks = $BricksMap
 onready var ball = $Ball
 onready var ballSmokes = $ParticlesBattery
 onready var cameraShake = $Camera2D/ScreenShake
+onready var scoreCounter = $HitCounter
+
+var currentScore := 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	scoreCounter.score = currentScore
 	ball.position = initialBallPos
 	ball.connect("ball_collided", self, "_on_ball_collided")
+	bricks.connect("brickDestroyed", self, "_on_brick_destroyed")
 
 
 func _on_ball_collided(collision: KinematicCollision2D):
@@ -19,3 +25,7 @@ func _on_ball_collided(collision: KinematicCollision2D):
 	var particlesPos = collision.position - (collision.normal * 10)
 	var particlesRotation = collision.normal.angle()
 	ballSmokes.fireNextParticleSystem(particlesPos, particlesRotation)
+
+
+func _on_brick_destroyed(type: int, pos: Vector2):
+	scoreCounter.score += 1000
--- game/gui/HitCounter.gd
+++ game/gui/HitCounter.gd
@@ -2,16 +2,33 @@ tool
 extends Node2D
 
 export(String) var scoreFormat: String = "%07d"
-export(float) var score: float = 0.0 setget _setScore
+export(float) var score: float = 0.0 setget _setValue
 
-onready var label: Label = $Label
+onready var valueLabel: Label = $HBoxContainer/ValueLabel
+onready var tween: Tween = $Tween
 
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-func _setScore(value: float):
-	score = value
-	if ($Label):
-		$Label.text = scoreFormat % score
+func _setValue(value: float):
+	setTotalValue(value)
+
+
+func setTotalValue(newTotal: float):
+	var prevScore: float = score
+	score = newTotal
+	if ($Tween):
+		$Tween.remove_all()
+		$Tween.interpolate_method(
+			self, "_setLabelTo",
+			prevScore, newTotal,
+			0.5, Tween.TRANS_QUAD, Tween.EASE_OUT
+		)
+		$Tween.start()
+
+
+func _setLabelTo(value: float):
+	if ($HBoxContainer/ValueLabel):
+		$HBoxContainer/ValueLabel.text = scoreFormat % value
