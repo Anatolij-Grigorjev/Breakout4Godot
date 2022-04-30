a222b85fb7b6241692f4cc2a61ccf44eeab1ab5c
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-04-30 10:38:32 +0300

    fix ball position reset in main game

--- game/BreakoutGame.gd
+++ game/BreakoutGame.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-export(Vector2) var initialBallPos = Vector2(190, 200)
-
 onready var bricks = $BricksMap
 onready var ball = $Ball
 onready var ballSmokes = $ParticlesBattery
@@ -14,17 +12,17 @@ var currentScore := 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scoreCounter.score = currentScore
-	ball.position = initialBallPos
 	ball.connect("ball_collided", self, "_on_ball_collided")
 	bricks.connect("brickDestroyed", self, "_on_brick_destroyed")
 
 
 func _on_ball_collided(collision: KinematicCollision2D):
 	
-	cameraShake.beginShake()
-	var particlesPos = collision.position - (collision.normal * 10)
-	var particlesRotation = collision.normal.angle()
-	ballSmokes.fireNextParticleSystem(particlesPos, particlesRotation)
+	if (collision.collider.is_in_group("bricks")):
+		cameraShake.beginShake()
+		var particlesPos = collision.position - (collision.normal * 10)
+		var particlesRotation = collision.normal.angle()
+		ballSmokes.fireNextParticleSystem(particlesPos, particlesRotation)
 
 
 func _on_brick_destroyed(type: int, pos: Vector2):
