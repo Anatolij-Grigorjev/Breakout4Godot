b2c47b5aa647a3a8ab70ee9a253c58c01ae63998
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-05-02 18:02:42 +0300

    make points rise when flashing

--- game/gui/ScoredPoints.gd
+++ game/gui/ScoredPoints.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 
 export(Array, Color) var colorsCycle = [Color.blue, Color.yellow]
-export(float) var colorFlashDuration = 0.25
-export(float) var totalFloatTime = 1.7
+export(float) var colorFlashDuration = 0.16
+export(float) var totalFloatTime = 1.0
+export(float) var pointsRaiseHeight: float = 25.5
 export(String) var text: String = "+100"
 
 onready var colorDurationTimer: Timer = $Duration
 onready var ttlTimer: TTL = $TTL
 onready var label: Label = $Label
+onready var tween: Tween = $Tween
 
 var currentColorIdx = 0
 
@@ -20,6 +22,7 @@ func _ready():
 	_refresh_label_color()
 	colorDurationTimer.start()
 	ttlTimer.start()
+	_start_raise_tween()
 
 
 
@@ -30,4 +33,14 @@ func _cycle_next_color():
 
 func _refresh_label_color():
 	label.modulate = colorsCycle[currentColorIdx]
+
+
+func _start_raise_tween():
+	tween.interpolate_property(
+		self, "position",
+		null, position - Vector2(0, pointsRaiseHeight),
+		totalFloatTime * 0.8, 
+		Tween.TRANS_QUAD, Tween.EASE_OUT
+	)
+	tween.start() 
 	
