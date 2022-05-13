48c7adfc0af1ec3021bf3d51288a33fbaba6dded
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-05-13 18:32:44 +0300

    add debug break brick in map

--- game/bricks/BricksMap.gd
+++ game/bricks/BricksMap.gd
@@ -17,10 +17,21 @@ func _ready():
 		animations_cache[tileIdx] = _build_hidden_boom_at_tile_idx(tileIdx)
 	
 
+func _process(delta):
+	if Input.is_action_just_released("debug1"):
+		var first_used_cell = get_used_cells()[0]
+		var rand_normal = Vector2(rand_range(-1.0, 1.0), rand_range(-1.0, 1.0))
+		_hit_brick_at_idx(first_used_cell, rand_normal)
+
+
 
 func bricks_hit_at(global_hit_pos: Vector2, hit_normal: Vector2):
 	var tileIdx: Vector2 = world_to_map(to_local(global_hit_pos))
 	print("global pos: " + String(global_hit_pos) + " tileidx: " + String(tileIdx) + " tile type: " + String(get_cellv(tileIdx)))
+	_hit_brick_at_idx(tileIdx, hit_normal)
+	
+
+func _hit_brick_at_idx(tileIdx: Vector2, hit_normal: Vector2):
 	var tileTypeAtPos = get_cellv(tileIdx)
 	if (tileTypeAtPos != TileMap.INVALID_CELL):
 		set_cellv(tileIdx, TileMap.INVALID_CELL)
@@ -30,7 +41,6 @@ func bricks_hit_at(global_hit_pos: Vector2, hit_normal: Vector2):
 		var map_cleared = _check_all_cells_empty()
 		if map_cleared:
 			emit_signal("map_cleared", total_num_bricks)
-	
 
 
 func _build_hidden_boom_at_tile_idx(idx: Vector2) -> Node2D:
