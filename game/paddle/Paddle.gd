d4b84ddbe0d1a94e70cd73dcfdd1615cfd41242b
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-05-20 18:28:03 +0300

    change from using static animationplayer to tween

--- game/paddle/Paddle.gd
+++ game/paddle/Paddle.gd
@@ -2,15 +2,17 @@ extends KinematicBody2D
 
 
 export(float) var speed: float = 150.0
+export(float) var base_ball_recoil = 7
 
 onready var ballSparks1x = $Sprite/ParticlesBattery
 onready var ballSparks2x = $Sprite/ParticlesBattery2x
 onready var ballSparks3x = $Sprite/ParticlesBattery3x
-onready var anim: AnimationPlayer = $AnimationPlayer
+onready var tween: Tween = $Tween
 onready var ballPosition: Position2D = $BallPosition
 
 
 var attachedBall: Node2D
+
 
 
 func _ready():
@@ -32,7 +34,7 @@ func _process(delta: float):
 
 
 func ball_hit_at(global_hit_pos: Vector2, ball_speed_coef: float):
-	anim.play("bounce_ball")
+	_prepare_and_run_bounce_reaction_tweens(ball_speed_coef)
 	var ballSparks = _pick_sparks_for_ball_hit(ball_speed_coef)
 	ballSparks.fireNextParticleSystem(global_hit_pos)
 
@@ -59,3 +61,24 @@ func _launch_ball():
 	get_parent().add_child(attachedBall)
 	attachedBall.global_position = Vector2(global_position.x, global_position.y - 10)
 	attachedBall = null
+
+
+func _prepare_and_run_bounce_reaction_tweens(ball_speed_coef: float):
+	tween.stop_all()
+	tween.remove_all()
+	var bounce_travel = base_ball_recoil * ball_speed_coef
+	var future_position = $Sprite.position + Vector2(0, bounce_travel)
+	tween.interpolate_property(
+		$Sprite, 'position', 
+		null, future_position, 
+		0.4, 
+		Tween.TRANS_QUAD, Tween.EASE_OUT
+	)
+	tween.interpolate_property(
+		$Sprite, 'position', 
+		future_position, Vector2.ZERO, 
+		0.3, 
+		Tween.TRANS_QUAD, Tween.EASE_IN, 
+		0.4
+	)
+	tween.start()
