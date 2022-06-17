67878ef1a8f2e9731e1e418aac307ec20c123d07
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-06-17 18:01:36 +0300

    remove individual sprites, move bricks into sheet

--- game/bricks/BricksMap.gd
+++ game/bricks/BricksMap.gd
@@ -46,11 +46,10 @@ func _hit_brick_at_idx(tile_idx: Vector2, hit_normal: Vector2):
 		return
 	
 	var next_brick_type = _get_next_brick_type(tile_type_at_idx)
+	_build_hidden_boom_at_tile_idx(tile_idx).explode(hit_normal)
 	if next_brick_type == TileMap.INVALID_CELL:
-		_build_hidden_boom_at_tile_idx(tile_idx).explode(hit_normal)
 		emit_signal("brickDestroyed", tile_type_at_idx, tile_idx)
 	else:
-		_build_hidden_boom_at_tile_idx(tile_idx).explode(hit_normal)
 		emit_signal("brick_damaged", tile_idx, tile_type_at_idx, next_brick_type)
 
 	yield(get_tree().create_timer(0.1), "timeout")
