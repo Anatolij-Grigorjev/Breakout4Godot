f02a535b0984ddf1a5cbcf91a741c4f9fa028382
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-07-23 12:13:13 +0300

    create powerup drops hierarchy

---
+++ game/drops/PowerupBase.gd
@@ -0,0 +1,36 @@
+tool
+extends Node2D
+
+
+export(float) var fall_rate = 150.0
+
+var should_move = false
+
+
+func start():
+	should_move = true
+
+
+func _process(delta):
+	if should_move:
+		position.y += fall_rate * delta
+
+
+
+func _on_Area2D_body_entered(body:Node):
+
+	if not body.is_in_group("paddle"):
+		return
+	
+	_paddle_collected_powerup()
+	queue_free()
+
+
+#override to make osmething happen when paddle collects powerup
+func _paddle_collected_powerup():
+	pass
+
+
+
+func _on_screen_exited():
+	queue_free()
--- game/drops/PowerupPoints.gd
+++ game/drops/PowerupPoints.gd
@@ -1,42 +1,16 @@
-tool
-extends Node2D
+extends "PowerupBase.gd"
 
 
 signal points_collected(amount)
 
-export(float) var fall_rate = 150.0
 export(float) var points_amount = 5000 setget _set_bonus_points
 
 
-var should_move = false
-
-
-
-func start():
-	should_move = true
-
-
-func _process(delta):
-	if should_move:
-		position.y += fall_rate * delta
-
-
-
-func _on_Area2D_body_entered(body:Node):
-
-	if not body.is_in_group("paddle"):
-		return
-	
+func _paddle_collected_powerup():
 	emit_signal("points_collected", points_amount)
-	queue_free()
 
 
 func _set_bonus_points(new_amount: float):
 	points_amount = new_amount
 	if $Sprite/AmountLbl:
 		$Sprite/AmountLbl.text = "+%d" % points_amount
-
-
-
-func _on_screen_exited():
-	queue_free()
