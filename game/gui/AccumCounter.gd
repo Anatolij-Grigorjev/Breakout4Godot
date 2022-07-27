e40cbdc45f42b2980065a0aa5a6e3d9c32738374
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-07-27 17:54:36 +0300

    add constant value grow rate

--- game/gui/AccumCounter.gd
+++ game/gui/AccumCounter.gd
@@ -4,6 +4,7 @@ extends Control
 export(String) var valueTagText: String = "SCORE: "
 export(String) var valueFormat: String = "%07d"
 export(float) var value: float = 0.0 setget _setValue
+export(float) var visualIncrementPerSecond: float = 2000
 
 onready var valueLabel: Label = $HBoxContainer/ValueLabel
 onready var tween: Tween = $Tween
@@ -21,11 +22,14 @@ func setTotalValue(newTotal: float):
 	var prevScore: float = value
 	value = newTotal
 	if ($Tween):
+		
+		var valueDiffference = abs(value - prevScore)
+		var incrementTimeSeconds = valueDiffference / visualIncrementPerSecond
 		$Tween.remove_all()
 		$Tween.interpolate_method(
 			self, "_setLabelTo",
 			prevScore, newTotal,
-			0.5, Tween.TRANS_QUAD, Tween.EASE_OUT
+			incrementTimeSeconds, Tween.TRANS_QUAD, Tween.EASE_OUT
 		)
 		$Tween.start()
 
