8439f13cb29a52a0f5cf581ae494015e529d7fe9
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-08-27 11:32:34 +0300

    steps to scale and center bricks correctly in stage selection

--- game/menus/StageSelectBlock.gd
+++ game/menus/StageSelectBlock.gd
@@ -19,6 +19,19 @@ func _ready():
 
 func _center_in_viewport(viewport: Viewport, bricks: BricksMap):
 
+	var scale_factor = _get_bricks_scale_factor(viewport, bricks)
+	var bricks_orig_size = bricks.get_used_rect().size * bricks.cell_size
+	var bricks_size = bricks_orig_size * scale_factor
+	var viewport_size = viewport.get_visible_rect().size
+	bricks.scale *= scale_factor
+	bricks.global_position = viewport_size / 2 - bricks_size / 2
+	print("bricks_orig_size=%s|\nscale_factor=%s|\nbricks_size=%s|\nviewport_size=%s|\nbricks_pos=%s|" % [
+		bricks_orig_size, scale_factor, bricks_size, viewport_size, bricks.global_position
+	])
+
+
+func _get_bricks_scale_factor(viewport: Viewport, bricks: BricksMap) -> float:
+	var viewport_size = viewport.get_visible_rect().size
 	var bricks_size = bricks.get_used_rect().size * bricks.cell_size
-	var viewport_visible = viewport.get_visible_rect()
-	bricks.global_position = Vector2(viewport_visible.position + viewport_visible.size / 2) - bricks_size / 2
+
+	return min(viewport_size.x / bricks_size.x, viewport_size.y / bricks_size.y)
